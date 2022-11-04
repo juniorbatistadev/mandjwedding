b8f66eab3396802d0a10dd1c9e4d1b775b51be85
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-11-04 19:57:54 -0400

    address

--- pages/directions.js
+++ pages/directions.js
@@ -5,6 +5,28 @@ function Directions() {
   return (
     <div>
       <Title text="Directions" />
+      <div>
+        <p>
+          The Golden Horseshoe Inn is located at: 9 Golden Horseshoe Rd
+          Stanardsville, VA 22973 HELPFUL
+        </p>
+        <p>
+          TIP: If using GPS / Navigation please type in our FULL street name
+          (there is also a Horseshoe Rd. in Stanardsville - so be sure not to
+          forget the "Golden" portion of our street name!)
+        </p>
+        <p>
+          FROM ROUTE 33 WEST: If you are traveling West on Route 33 - look for
+          the Lydia Log Cabin building on your LEFT with the RED TIN ROOF - we
+          are the very next LEFT on ROUTE 625. If you pass a sign that says "Now
+          Entering Shenandoah National Park" you have gone too far.
+        </p>
+        <p>
+          FROM ROUTE 33 EAST: If you are heading East on Route 33 we are the
+          first RIGHT after coming down the mountains and passing the sign that
+          says "Now Leaving Shenandoah National Park"
+        </p>
+      </div>
     </div>
   );
 }
