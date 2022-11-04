0ba866876de9c178d8e67bd905cefac40a87ac57
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-11-04 19:47:17 -0400

    schedule

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,10 +1,37 @@
 import React from "react";
 import Title from "../src/components/Title";
+import styles from "../styles/schedule.module.css";
 
 function Schedule() {
   return (
     <div>
       <Title text="Schedule" />
+      <div>
+        <div className={styles.event}>
+          <p className={styles.time}>1:00 pm</p>
+          <p className={styles.name}>Ceremony</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.time}>2:00 pm</p>
+          <p className={styles.name}>Happy Hour</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.time}>3:00 pm</p>
+          <p className={styles.name}>First Dance</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.time}>4:00 pm</p>
+          <p className={styles.name}>Speaches</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.time}>5:00 pm</p>
+          <p className={styles.name}>Cake Cutting</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.time}>6:00 pm</p>
+          <p className={styles.name}>Send off</p>
+        </div>
+      </div>
     </div>
   );
 }
