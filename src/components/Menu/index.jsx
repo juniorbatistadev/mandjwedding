415ece08f13fac3694f6d4958d212cd1a3dcb277
Junior Batista <juniorbatistadev@gmail.com>
2023-05-18 20:41:16 -0400

    removed rsvp

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -22,9 +22,9 @@ function Menu() {
       <li className={router.pathname == "/directions" ? styles.active : ""}>
         <Link href="/directions">Directions</Link>
       </li>
-      <li className={router.pathname == "/rsvp" ? styles.active : ""}>
+      {/* <li className={router.pathname == "/rsvp" ? styles.active : ""}>
         <Link href="/rsvp">RSVP</Link>
-      </li>
+      </li> */}
       <li className={router.pathname == "/registry" ? styles.active : ""}>
         <Link href="/registry">Registry</Link>
       </li>
